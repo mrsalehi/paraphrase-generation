4efbb1d790c10fc1e5b7186643a207302b0e4660
Amirhosein Kazemnejad <ub.maka@gmail.com>
2018-11-16 23:24:30 +0330

    Add graph utils from google seq2seq github

---
+++ models/common/graph_utils.py
@@ -0,0 +1,42 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+
+
+def add_dict_to_collection(dict_, collection_name):
+    """Adds a dictionary to a graph collection.
+    Args:
+      dict_: A dictionary of string keys to tensor values
+      collection_name: The name of the collection to add the dictionary to
+    """
+    key_collection = collection_name + "_keys"
+    value_collection = collection_name + "_values"
+    for key, value in dict_.items():
+        tf.add_to_collection(key_collection, key)
+        tf.add_to_collection(value_collection, value)
+
+
+def get_dict_from_collection(collection_name):
+    """Gets a dictionary from a graph collection.
+    Args:
+      collection_name: A collection name to read a dictionary from
+    Returns:
+      A dictionary with string keys and tensor values
+    """
+    key_collection = collection_name + "_keys"
+    value_collection = collection_name + "_values"
+    keys = tf.get_collection(key_collection)
+    values = tf.get_collection(value_collection)
+    return dict(zip(keys, values))
