ba09635f553f5333a326bbc3b1332c15f55e246f
Amirhosein Kazemnejad <ub.maka@gmail.com>
2018-11-21 23:03:43 +0330

    Fix

--- models/common/util.py
+++ models/common/util.py
@@ -3,6 +3,9 @@ import subprocess
 
 def get_num_total_lines(p):
     # result = subprocess.run(['wc', '-l', p], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    if not isinstance(p, str):
+        p = str(p)
+
     result = subprocess.check_output(['wc', '-l', p])
     if isinstance(result, bytes):
         result = result.decode('utf-8')
