071a2cadb191054588e712d68bb7b2817d89deed
amirhosein <ub.maka@gmail.com>
2019-05-14 16:04:09 +0430

    Add new models

---
+++ models/im_aeradp_cnt_shr_add_sub_com_wa/__init__.py
@@ -0,0 +1,50 @@
+import models.im_attn_ee_rnn_attn_dec_pg.model as base_model
+import models.neural_editor as base
+import models.neural_editor.agenda as base_agenda
+import models.neural_editor.decoder as base_decoder
+import models.neural_editor.input as base_input
+import models.neural_editor.optimizer as base_optimizer
+import models.neural_editor.paraphrase_gen as base_para_gen
+from models.im_aeradp_cnt_shr_add_sub.editor import editor_train
+from models.im_aeradp_cnt_shr_add_sub_com_wa.input import read_examples_from_file, input_fn, input_fn_from_gen_multi
+from models.im_attn_ee_rnn_attn_dec_copy_net.paraphrase_gen import create_formulas
+from models.im_attn_ee_rnn_attn_dec_pg.decoder import create_decoder_cell
+from models.im_attn_ee_rnn_attn_dec_pg.model import model_fn
+from models.im_attn_ee_rnn_attn_dec_pg.optimizer import loss
+from models.im_cnct_agn import concat
+
+NAME = 'im_aeradp_cnt_shr_add_sub_com_wa'
+
+base_model.editor.editor_train = editor_train
+base_agenda.linear = concat
+
+base_decoder.create_decoder_cell = create_decoder_cell
+base_optimizer.loss = loss
+base_input.read_examples_from_file = read_examples_from_file
+base_input.input_fn = input_fn
+base_para_gen.create_formulas = create_formulas
+base_para_gen.input_fn_from_gen_multi = input_fn_from_gen_multi
+
+
+def train(*args):
+    return base.train(*args, my_model_fn=model_fn)
+
+
+def eval(*args):
+    return base.eval(*args, my_model_fn=model_fn)
+
+
+def predict(*args):
+    return base.predict(*args, my_model_fn=model_fn)
+
+
+def augment_meta_test(*args):
+    return base.augment_meta_test(*args, my_model_fn=model_fn)
+
+
+def augment_debug(*args):
+    return base.augment_debug(*args, my_model_fn=model_fn)
+
+
+def generate_paraphrase(*args):
+    return base.generate_paraphrase(*args, my_model_fn=model_fn)
